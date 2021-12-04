f85782a16281ae0ca4ebec8a60b47df11babe068
Roberto Achar <robertoachar@gmail.com>
2021-12-04 09:56:06 -0300

    feature: add electron

---
+++ src/main.js
@@ -0,0 +1,37 @@
+/* eslint import/no-extraneous-dependencies: off */
+
+import { app, BrowserWindow } from 'electron';
+import startup from 'electron-squirrel-startup';
+import { environment } from './environment';
+
+if (startup) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 768,
+    webPreferences: {
+      preload: environment.MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+    },
+  });
+
+  win.loadURL(environment.MAIN_WINDOW_WEBPACK_ENTRY);
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
