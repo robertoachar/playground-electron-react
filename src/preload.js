4358b64c15f8c80f7ec67ce4552602e6624dfb86
Roberto Achar <robertoachar@gmail.com>
2022-01-06 20:29:50 -0300

    refactor: change to arrow function

--- src/preload.js
+++ src/preload.js
@@ -4,7 +4,7 @@ contextBridge.exposeInMainWorld('electron', {
   hello: () => {
     console.log('preload');
   },
-  on(channel, func) {
+  on: (channel, func) => {
     const channels = ['ping'];
     if (channels.includes(channel)) {
       ipcRenderer.once('ping', (event, ...args) => func(...args));
